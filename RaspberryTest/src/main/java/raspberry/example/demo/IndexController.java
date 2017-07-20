f9d3eff8f34d916db75cd389b9043ecc5e5b2c25
teliseeva <tatiana,zuyeva@gmail.com>
2017-07-20 22:48:47 +0300

    Added routing

--- RaspberryTest/src/main/java/raspberry/example/demo/IndexController.java
+++ RaspberryTest/src/main/java/raspberry/example/demo/IndexController.java
@@ -6,14 +6,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class IndexController {
 	
-	@RequestMapping("/temperature")
-	 public long GetTemperature() {
-		 return Math.round(20 + Math.random()*10);
-	 }
+	public static class TemHumSensor{
+		public long Temperature;
+		public long Humidity;
+	}
 	
-	@RequestMapping("/humidity")
-	 public long GetHumidity() {
-		 return Math.round(50 + Math.random()*10);
-	 }
-
+	@RequestMapping("/tempHum")
+	 public TemHumSensor GetTemperature() {
+		 TemHumSensor data = new TemHumSensor();
+		 data.Temperature = Math.round(20 + Math.random()*10);
+		 data.Humidity = Math.round(50 + Math.random()*10);
+		 return data;
+	 }	
 }
+
+
--- RaspberryTest/src/main/resources/static/App/app.js
+++ RaspberryTest/src/main/resources/static/App/app.js
@@ -1,16 +1,30 @@
 (function(){
 	'use strict'
-	var app = angular.module('app', []);
-	app.controller('AppController', function($scope, $http) {
+	var app = angular.module('app', ['ngRoute', 'ngResource']);
+	
+	app.config(function($routeProvider){
+		$routeProvider.when('/TempHum',{
+			templateUrl: 'templates/TemperatureHumidity.html',
+			controller: 'TempHumController'
+		})
+		.when('/Gas',{
+			templateUrl: 'templates/Gas.html',
+			controller: 'gasController'
+		})
+	})
+	
+	
+	app.controller('TempHumController', function($scope, $http) {
 		$scope.getData = function(){
-			$http.get('/temperature')
+			$http.get('/tempHum')
 			.then(function(response){
-				$scope.temperature = response.data;
-			});
-			$http.get('/humidity')
-			.then(function(response){
-				$scope.humidity = response.data;
-			});					
+				$scope.temperature = response.data.Temperature;
+				$scope.humidity = response.data.Humidity;
+			});				
 		}		
-	});		
+	});
+	
+	app.controller('gasController', function($scope) {
+
+	});
 })();
